9c4d1f4fc5ff5ce3f98d379403f6603fa1762f06
Eric Miller <ericmiller901@gmail.com>
2022-08-29 21:11:08 -0500

    Initial upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "raylib.h"
+
+int main(int argc, char)
+{
+    // Window dimensions
+    const int window_width = 640;
+    const int window_height = 480;
+
+    while(!WindowShouldClose())
+    {
+        
+    }
+}
