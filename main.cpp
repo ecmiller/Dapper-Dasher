128916d65bc6e6f6e93314d51d573becc18049da
Eric Miller <ericmiller901@gmail.com>
2022-08-29 21:51:12 -0500

    Gravity simulation and jump functionality

--- main.cpp
+++ main.cpp
@@ -1,13 +1,67 @@
 #include "raylib.h"
 
-int main(int argc, char)
+int main()
 {
     // Window dimensions
-    const int window_width = 640;
-    const int window_height = 480;
+    const int window_width{640};
+    const int window_height{480};
 
+    // Rectangle dimensions
+    const int width{50};
+    const int height{80};
+
+    int posY{window_height - height};
+
+    bool isInAir{false};
+
+    int velocity{0};
+    // Acceleration due to gravity ((pixels / frame) / frame)
+    const int gravity{1};
+    const int jumpVelocity{-20};
+
+    // Initialize the window
+    InitWindow(window_width, window_height, "Dapper Dasher");
+
+    SetTargetFPS(60);
     while(!WindowShouldClose())
     {
+        // Start drawing
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        // Ground check
+        if(posY >= window_height - height)
+        {
+            // Rectangle is on the ground
+            isInAir = false;
+            // Reset velocity
+            velocity = 0;
+        }
+        else 
+        {
+            // Rectangle is in the air
+            isInAir = true;
+            // Apply gravity
+            velocity += gravity;
+        }
+
+        // Detect jump input (space key)
+        if(IsKeyPressed(KEY_SPACE)){
+            if(!isInAir)
+            {
+                // Apply jump velocity
+                velocity += jumpVelocity;
+            }
+        }
+
+        // Update position
+        posY += velocity;
+
+        // Render the Player (rectangle)
+        DrawRectangle(window_width/2, posY, width, height, BLUE);
         
+        // Stop drawing
+        EndDrawing();
     }
-}
+    CloseWindow();
+} 
